0a19237e46e848651669aa177f5452e9bb9dbcae
Patryk Strużek <patrykstruzek17@gmail.com>
2024-03-18 19:32:29 +0100

    Replace mongodb with rusqlite
    because I need local low dependency db

--- src/db.rs
+++ src/db.rs
@@ -1,27 +1,13 @@
-use mongodb::{options::ClientOptions, Client, Collection};
-
+use rusqlite::*;
 use crate::room::{ChatRoom, Message};
 
 const FILEPATH: &str = " ";
 
-pub struct ChatDb {
-    db_collection: Collection<Message>,
-}
+pub struct ChatDb {}
 
 impl ChatDb {
     pub fn new() -> Self {
-        let mut client_options = ClientOptions::parse("mongodb://localhost:27017").unwrap();
-
-        client_options.app_name = Some("gigachat".to_string());
-        client_options.db_path = Some(FILEPATH.to_string());
-
-        let client = Client::with_options(client_options).unwrap();
-
-        let db = client.database("gigachatdb");
-
-        ChatDb {
-            db_collection: db.collection("messages"),
-        }
+        Self {}
     }
 
     pub fn get_chatroom(room_id: String) {}
